b6bf7cefbbcc22d8397fd5bb903ce4ef02a8d880
Acemany <116539353+acemany@users.noreply.github.com>
2024-04-11 12:28:03 +0300

    Hmmmm

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# pygame SQL editor
- Why not?
+# python tsv to json converter
+
+converts json files to tsv or tsv files to json
+
+can convert json:
+
+    [
+        {
+            "name": "John",
+            "age": 17
+        },
+        {
+            "name": "Micle",
+            "age": 24
+        }
+    ]
+
+to tsv:
+
+    name    age
+    John    17
+    Micle   24
+
+and vice versa
---
+++ main.py
@@ -0,0 +1,23 @@
+from typing import Tuple, Dict
+from json import loads
+from sys import argv
+
+
+def tsv_to_json(tsv: str) -> Tuple[Dict[str, str | int]]:
+    return [{tsv.split("\n")[0].split("\t")[index]: int(word) if word.isdecimal() else word
+             for index, word in enumerate(line.split("\t"))}
+            for line in tsv.split("\n")[2:]]
+
+
+def json_to_tsv(json: Tuple[Dict[str, str | int]]) -> Tuple[Tuple[str | int]]:
+    return "\n".join(["\t".join(json[0].keys())] + ["\t".join([str(value)
+                                                               for value in dict.values()])
+                                                    for dict in json])
+
+
+if __name__ == "__main__":
+    with open(argv[1], "r", encoding="utf-8") as f:
+        formnew = "tsv" if argv[1].split(".")[-1] == "json" else "json"
+        with open(f"{argv[1].split(".")[0]}.{formnew}", "w", encoding="utf-8") as r:
+            r.write(str(tsv_to_json(f.read()) if formnew == "json" else
+                        json_to_tsv(loads(f.read()))).replace("'", '"'))
